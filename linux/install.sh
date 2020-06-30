594dcbbfdb9fbb9006e96e5d9a4fe6a0a9f663fb
Fabio Polanco <57909981+fabiopolancoe@users.noreply.github.com>
2020-06-30 01:41:37 -0500

    update install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -1,7 +1,5 @@
 echo Welcome to LuaCon Installer
 echo
-echo Installing Love2D
-sudo apt-get install love
 sudo mkdir /usr/bin/luacon-f
 sudo cp luacon.lua luacon_config luacon_utils /usr/bin/luacon-f
 sudo cp luacon.sh /usr/bin; sudo mv /usr/bin/luacon.sh /usr/bin/luacon
